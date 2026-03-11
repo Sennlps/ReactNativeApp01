b71fd8c8fff9f813fc77f1dd76760839a25b0ddd
Sennlps <180540662+Sennlps@users.noreply.github.com>
2026-03-11 14:16:29 +0100

    navbar, routes, nativewind, tailwind, metro, babel,

---
+++ Movie/app/onboarding.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Onboarding() {
+  return (
+    <View style={styles.container}>
+      <Text>Onboarding</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
